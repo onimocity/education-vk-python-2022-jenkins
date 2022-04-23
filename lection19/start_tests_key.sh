62e574da31ea32d6836d13e8426f487dc40f79f2
Konstantin Ermakov <konstant.ermakov@yandex.ru>
2022-04-23 20:58:07 +0300

    fix
    fix

--- lection19/start_tests_key.sh
+++ lection19/start_tests_key.sh
@@ -3,7 +3,7 @@
 cd code
 
 
-cmd="pytest -s -l -v ${TESTS_PATH} -n ${THREADS:-2} --alluredir /tmp/allure"
+cmd="pytest -s -l -v ${TESTS_PATH} -n ${THREADS:-2} --alluredir ${WORKSPACE}/alluredir"
 
 if [ -n "${KEYWORD}" ]; then
     cmd="$cmd -k ${KEYWORD}"
