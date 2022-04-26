93559f49ee3fba87aa2eb0dde1e49b95062437d0
Konstantin Ermakov <konstant.ermakov@yandex.ru>
2022-04-26 23:55:00 +0300

    add test_second
    test_second add

---
+++ lection19/code/tests/test_second.py
@@ -0,0 +1,19 @@
+
+
+def test_first():
+
+    assert 1 == 1
+
+
+def test_second():
+
+    assert 'abc' == 'abc'
+
+def test_third():
+
+    assert 5 > 2
+
+
+def test_fourth():
+
+    assert 'abc' != 'aaa'
